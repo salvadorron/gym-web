0b0075c8d318bda32b52ec35a3abeb454eadcbaf
Salvador Ron <salvadorronosio@gmail.com>
2025-01-07 14:43:41 -0400

    fix training

--- app/planes/layout.tsx
+++ app/planes/layout.tsx
@@ -1,11 +1,15 @@
 import { getClient } from "@/lib/data";
 import Toolbar from "../ui/toolbar";
 import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export default async function MembershipLayout({ children }: { children: React.ReactNode }) {
 
     const session = await auth() as any // eslint-disable-line @typescript-eslint/no-explicit-any
     const client = await getClient(session?.user.client.id)
+
+    if(client.plans.length === 0) redirect('/planes');
+    
 
     return (
         <div className="">
