8a2fd798a4bc0cac5a3d0f701ab028f2805575ab
Salvador Ron <salvadorronosio@gmail.com>
2025-02-13 17:53:02 +0000

    fix types

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        'localhost:3000',
-        'https://special-system-4vwp7qwgwgx2jq5w-4200.app.github.dev/'
-      ]
-    }
-  },
   images: {
     remotePatterns: [
       {
