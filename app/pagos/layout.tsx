6cfbde8a27fbade5f83f5aa856273443345073f5
Salvador Ron <salvadorronosio@gmail.com>
2025-02-14 17:48:23 -0400

    fix pay

--- app/pagos/layout.tsx
+++ app/pagos/layout.tsx
@@ -1,10 +1,22 @@
 import { auth } from "@/auth";
 import Header from "@/components/ui/header";
 import { ProtectedRouter } from "@/components/ui/protected-router";
+import { getClient } from "@/lib/data";
+import { redirect } from "next/navigation";
 
 export default async function PaymentLayout({ children }: { children: React.ReactNode }) {
 
     const session = await auth();
+
+
+        const client = await getClient(session?.user.client?.id);
+    
+            if(session?.user.roleId === 'client'){
+                if(client?.plan === null){
+                    redirect('/planes')
+                }
+                
+            }
 
     return (
             <ProtectedRouter roleId={session?.user.roleId} roles={['client']}>
